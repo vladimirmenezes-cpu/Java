cdb3a6795b6b8c2951c826c03e8b115e6aa63420
Aluno Dendezeiros <aluno.den@senaibahia.com.br>
2026-08-14 15:47:56 -0300

    segunda aula parte 2

--- aula02/atividade/Desafio1Produto.java
+++ aula02/atividade/Desafio1Produto.java
@@ -1,18 +1,26 @@
 package aula02.atividade;
 
+import java.util.Scanner;
+
 public class Desafio1Produto {
-    
+
     public static void main(String[] args) {
-        
-        String produto="faca do CHEF";
-        int quantidade=10;
-        double preco=100.50;
 
-        System.out.printf("produto: %s%n",produto); 
-        System.out.printf("Quantidade: %s%n",quantidade); 
-        System.out.printf("Preço unitário: %s%n",preco); 
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite o produto: ");
+        String produto = scanner.nextLine();
+
+        System.out.print("Digite a quantidade: ");
+        int quantidade = scanner.nextInt();
+
+        System.out.print("Digite o preço: ");
+        double preco = scanner.nextDouble();
+
+        System.out.printf("%nProduto: %s%n", produto);
+        System.out.printf("Quantidade: %d%n", quantidade);
+        System.out.printf("Preço unitário: R$ %.2f%n", preco);
+
+        scanner.close();
     }
-
-
-    }
-
+}
