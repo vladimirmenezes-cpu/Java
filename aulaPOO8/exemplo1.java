5c071377120fdd9103a2100cd94848b721cd0407
FelipeGit-codes <andre.f.santos8@ba.estudante.senai.br>
2026-09-10 13:54:53 -0300

    POO

---
+++ aulaPOO8/exemplo1.java
@@ -0,0 +1,29 @@
+package aulaPOO8;
+
+public class exemplo1 {
+
+//sem parametro e sem retorno 
+    public static void main(String[] args) {
+        System.out.println("Seja bem vindo");
+    }
+
+//com parametro e sem retorno 
+    public static void mostrarNome(String nome) {
+        System.out.println("nome: "+nome);
+    }
+
+//sem parametro e com retorno
+    public  static  int obterNome(){
+        return  10;
+    }
+
+
+//com parametro e com retorno
+public  static  int somar(int n1, int n2){
+    return  n1+n2;
+}
+
+
+
+
+}
