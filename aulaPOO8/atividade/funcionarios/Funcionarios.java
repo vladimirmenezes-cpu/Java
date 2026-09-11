35840b7006676480df8ed26c859b3bdcebe58fe9
Vladimir menezes <vladimir.menezes@ba.estudante.senai.br>
2026-09-11 16:08:53 -0300

    teste de commit

--- aulaPOO8/atividade/funcionarios/Funcionarios.java
+++ aulaPOO8/atividade/funcionarios/Funcionarios.java
@@ -16,4 +16,14 @@ public class Funcionarios {
     public void setNome(String nome){
         this.nome = nome;
     }
+
+    public  double getSalario(){
+        return salario;
+    }
+
+    public void setSalario(double salario){
+        this.salario = salario;
+    }
+
+    
 }
--- aulaPOO8/atividade/veiculos/VeiculoApp.java
+++ aulaPOO8/atividade/veiculos/VeiculoApp.java
@@ -1,6 +1,5 @@
 package aulaPOO8.atividade.veiculos;
 
-import java.lang.classfile.instruction.SwitchCase;
 import java.util.Scanner;
 
 public class VeiculoApp {
@@ -8,15 +7,20 @@ public class VeiculoApp {
         Scanner sc = new Scanner(System.in);
         int op;
 
+        VeiculoCarro vc = null;
+        VeiculoMoto vm = null;
+
         do {
-            System.out.println("\n=== Sistema de cadastro de veiculos ===");
-            System.out.println("1 - Moto");
-            System.out.println("2 - Carro");
-            System.out.println("0 - Sair do programa");
-            System.out.println("Escolha a opção: ");
+            System.out.println("\n>>> MENU DE VEÍCULOS <<<");
+            System.out.println("1 - Cadastrar Carro");
+            System.out.println("2 - Cadastrar Moto");
+            System.out.println("3 - Exibir dados do carro");
+            System.out.println("4 - Exibir dados da moto");
+            System.out.println("0 - Sair");
+            System.out.print("Escolha a opção: ");
 
             while (!sc.hasNextInt()) {
-                System.out.println("Opção invalida! Digite 0,1 ou 2");
+                System.out.println("Opção inválida! Digite um número de 0 a 4.");
                 sc.next();
             }
             op = sc.nextInt();
@@ -24,47 +28,64 @@ public class VeiculoApp {
 
             switch (op) {
                 case 1: {
-                    System.out.println("\n--- Cadastro de moto ---");
-                    System.out.print("Informe a Marca: ");
-                    String marca = sc.nextLine();
-
-                    System.out.println("Informe o ano: ");
-                    int ano = sc.nextInt();
-
-                    System.out.println("informe a Cilindrada");
-                    float cilindrada = sc.nextFloat();
-
-                    VeiculoMoto vm = new VeiculoMoto(marca, ano, cilindrada);
-                    System.out.println("\n--- Dados da moto cadastrada ---");
-                    vm.exibirDados();
-                    break;
-                }
-                case 2: {
                     System.out.println("\n--- Cadastro de carro ---");
                     System.out.print("Informe a marca: ");
                     String marca = sc.nextLine();
 
-                    System.out.println("Informe o ano: ");
+                    System.out.print("Informe o ano: ");
                     int ano = sc.nextInt();
 
-                    System.out.println("Informe a quantidade de portas");
+                    System.out.print("Informe a quantidade de portas: ");
                     int qntportas = sc.nextInt();
+                    sc.nextLine();
 
-                    VeiculoCarro vc = new VeiculoCarro(marca, ano, qntportas);
-                    System.out.println("\n--- Dados do carro cadastrado ---");
-                    vc.exibirDados();
+                    vc = new VeiculoCarro(marca, ano, qntportas);
+                    System.out.println("Carro cadastrado com sucesso!");
+                    break;
+                }
+                case 2: {
+                    System.out.println("\n--- Cadastro de moto ---");
+                    System.out.print("Informe a marca: ");
+                    String marca = sc.nextLine();
+
+                    System.out.print("Informe o ano: ");
+                    int ano = sc.nextInt();
+
+                    System.out.print("Informe a cilindrada: ");
+                    float cilindrada = sc.nextFloat();
+                    sc.nextLine();
+
+                    vm = new VeiculoMoto(marca, ano, cilindrada);
+                    System.out.println("Moto cadastrada com sucesso!");
+                    break;
+                }
+                case 3: {
+                    System.out.println("\n--- Dados do Carro ---");
+                    if (vc != null) {
+                        vc.exibirDados();
+                    } else {
+                        System.out.println("Nenhum carro cadastrado ainda!");
+                    }
+                    break;
+                }
+                case 4: {
+                    System.out.println("\n--- Dados da Moto ---");
+                    if (vm != null) {
+                        vm.exibirDados();
+                    } else {
+                        System.out.println("Nenhuma moto cadastrada ainda!");
+                    }
                     break;
                 }
                 case 0:
                     System.out.println("Saindo do sistema...");
                     break;
-            
+
                 default:
-                    System.out.println("Opção invalida! tente novamente.");
+                    System.out.println("Opção inválida! Tente novamente.");
                     break;
             }
-        
-        
+
         } while (op != 0);
 
         sc.close();
