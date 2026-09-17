f343f34a3c7cf6fa56959f95e501d6a1bb3eba8c
Vladimir menezes <vladimir.menezes@ba.estudante.senai.br>
2026-09-17 14:32:39 -0300

    Cliente pagamento

--- aula10/pessoa/Cliente.java
+++ aula10/pessoa/Cliente.java
@@ -3,6 +3,33 @@ package aula10.pessoa;
 import aula10.Pagamento.Pagamento;
 
 public class Cliente extends Pessoa implements Pagamento {
+
+    public Cliente(String nome){
+        super(nome);
+    }
+
+    @Override 
+    public void pagar(double valor){
+        System.out.println("Pagamento realizad: R$" +valor);
+    }
+
+    public void comprar(String produto){
+        System.out.println("Produto comprado" +produto);
+    }
+
+    public void comprar(String produto, int quantidade){
+        System.out.println("Produto comprado: " +produto);
+        System.out.println("Quantidade: " +quantidade);
+    }
+
+    public void comprar(String produto, int quantidade, double valor){
+        System.out.println("Produto comprado: " +produto);
+        System.out.println("Quantidade:" +quantidade);
+        System.out.println("Valor unitario:" +valor);
+        System.out.println("Total: R$" +(quantidade*valor));
+    }
     
     
 }
+    
+
